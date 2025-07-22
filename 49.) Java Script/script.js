cb7e87241130c42e2eb8e02650d916c37c7da70a
ZainWajid775 <u2023775@giki.edu.pk>
2025-07-23 02:09:18 +0500

    49

---
+++ 49.) Java Script/script.js
@@ -0,0 +1,16 @@
+alert ("Hello World");
+
+console.log("Console")
+console.log("Code........")
+
+var a = prompt("Prompt")
+var isTure = prompt("Leave")
+
+if(isTure){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
+
+console.log("Number" + a )
