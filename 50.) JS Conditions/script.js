3f22f1ab323eb3ad69163678f120caf3d0516e43
ZainWajid775 <u2023775@giki.edu.pk>
2025-07-23 20:28:11 +0500

    50

---
+++ 50.) JS Conditions/script.js
@@ -0,0 +1,17 @@
+console.log("Hello World")
+
+let age = 45;
+let grace = 2;
+
+console.log(age+grace);
+
+if(age>18){
+    console.log("You are an adult");
+}
+else if(age == 45)
+{
+    console.log("You are 45");
+}
+else{
+    console.log("You are not an adult");
+}
