7682fc1cf1cb789eb0b539d4509d3d744af999da
ZainWajid775 <u2023775@giki.edu.pk>
2025-07-24 16:10:02 +0500

    54

---
+++ 54.) JS Strings/script.js
@@ -0,0 +1,12 @@
+let a = "Zain";
+
+console.log(a[0]);
+console.log(a.length);
+
+let f = "Temp";
+
+console.log("Name : " + a + " Temp : " + f);
+
+console.log(a.toLowerCase());
+console.log(a.slice(0,3));
+console.log(a.replace("Z" , "Y"));
