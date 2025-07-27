af1128f1cf4a0281a49a557fae80e8f6ba81bb5b
ZainWajid775 <u2023775@giki.edu.pk>
2025-07-27 13:58:12 +0500

    60

---
+++ 60.) JS Ids, Classe etc/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+//let a = document.getElementsByClassName("box")
+//console.log(a)
+//a[2].style.backgroundColor = "red"
+
+//document.getElementById("red").style.backgroundColor = "red"
+
+//document.querySelector(".box").style.backgroundColor = "green"
+
+console.log(document.querySelectorAll(".box"))
+
