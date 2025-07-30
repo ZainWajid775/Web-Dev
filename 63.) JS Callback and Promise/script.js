15e0f58554ad1f83560a7556cac1c536dec630a2
ZainWajid775 <u2023775@giki.edu.pk>
2025-07-30 14:11:32 +0500

    63

--- 63.) JS Callback and Promise/script.js
+++ 63.) JS Callback and Promise/script.js
@@ -1,7 +1,26 @@
 console.log("Hello")
 
 setTimeout(() =>{
-    console.log("World")
+    console.log("Timeout 1")
 }, 2000);
 
+setTimeout(() =>{
+    console.log("Timeout 2")
+}, 2000);
+
+
 console.log("After func")
+
+const callback = (arg) =>{
+    console.log(arg);
+}
+
+const loadScript = (src, callback)=>{
+    let sc = document.createElement("script");
+
+    sc.src = src;
+    sc.onload = callback("Harry");
+    document.head.append(sc)
+}
+
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js" , callback)
